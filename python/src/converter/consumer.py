11b29f48ff942e4ec3cacdf791fe99fd218f815b
seansuen90 <seansuen90@gmail.com>
2025-10-18 13:13:51 +0000

    .

--- python/src/converter/consumer.py
+++ python/src/converter/consumer.py
@@ -4,7 +4,7 @@ import gridfs
 from convert import to_mp3
 
 def main():
-    client = MongoClient('host.minikube.internal', 27017)
+    client = MongoClient('mongo', 27017)
     db_videos = client.videos
     db_mp3s = client.mp3s
     fs_videos = gridfs.GridFS(db_videos)
